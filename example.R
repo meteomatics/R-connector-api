e30609760a3c3f6f897b297789aaa5ee40ec4ef8
Jonas Lauer <jonaslauer@jonass-mbp-2.home>
2019-08-27 10:39:52 +0200

    Add new functionalities (Pivot Grid, Timeseries Grid, Lightning Strikes, NETCDF, Grads plots, Station List, Station Timeseries, Lates Init Times, Available Time Ranges) and restructure connector code.

--- example.R
+++ example.R
@@ -1,46 +1,161 @@
-#Meteomatics Weather API Connector
-#Connecting with the query_api_time.R
+#Meteomatics Weather API Connector - Examples
+
+
+#Required packages: httr, data.table, lubridate, ggplot2, stringr.
+#install.packages("")
+
 source('R_query_api.R')
 
-#Data
-username = "r-community"
-password = "Utotugode673"
 
-time_zone = "Europe/Berlin"
-startdate = ISOdatetime(year = strtoi(strftime(today(),'%Y')),
-                        month = strtoi(strftime(today(),'%m')),
-                        day = strtoi(strftime(today(),'%d')),
-                        hour = 00, min = 00, sec = 00, tz = "UTC")
-enddate = ISOdatetime(year = strtoi(strftime(today(),'%Y')),
-                      month = strtoi(strftime(today(),'%m')),
-                      day = strtoi(strftime(today(),'%d'))+1,
-                      hour = 00, min = 00, sec = 00, tz = "UTC")
-interval = "PT1H"
+#Account data
+username <- "r-community"
+password <- "Utotugode673"
 
 #Find out what the current account can do
-limits = query_user_features(username, password)
+limits <- query_user_features(username, password)
+limits
 
-#Query a timeseries in a single point for two parameters
-cat("\nPerforming time series query.\n")
-request_type= "timeseries"
-parameters = "t_2m:C,relative_humidity_1000hPa:p" #different parameters
-coordinate = "47.11,11.47" #Point or line
-ts_output = query_api(username, password, startdate, enddate, interval, parameters, coordinate, request_type, time_zone=time_zone)
-print(ts_output)
+#Set initial time data####
+time_zone <- "Europe/Berlin"
+startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                        month = as.integer(strftime(today(),'%m')), 
+                        day = as.integer(strftime(today(),'%d')), 
+                        hour = 00, min = 00, sec = 00, tz = "UTC")
+enddate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                      month = as.integer(strftime(today(),'%m')), 
+                      day = as.integer(strftime(today(),'%d'))+1, 
+                      hour = 00, min = 00, sec = 00, tz = "UTC")
+interval <- "PT1H"
 
-#Run a grid query if possible
 
-if (limits['area request option'])
-{
-  cat("\nPerforming domain query.\n")
-  parameters = "t_2m:C" #only one parameter
-  coordinate = "48.5,5.5_45.5,11.5:800x600" #Rectangle
-  request_type = "domain"
-  grid_output = query_api(username, password, startdate, enddate, interval, parameters, coordinate, request_type, time_zone=time_zone)
-  print(grid_output)
-} else
-{
-  cat(sprintf("\nYour account '%s' does not include area requests.
-With the corresponding upgrade you could query whole grids of data at once or even time series of grids.
-Please check http://shop.meteomatics.com or contact us at shop@meteomatics.com for an individual offer.", username))
-}
+#Query a Timeseries####
+
+parameters <- "t_2m:C,relative_humidity_1000hPa:p"
+coordinate <- "47.11,11.47"
+#coordinate <- "47.11,11.47+50,10"
+ts_output <- timeseries(startdate, enddate, interval, parameters, coordinate)
+head(ts_output)
+
+
+#Grid####
+
+parameter <- "t_2m:C"
+coordinate <- "48.5,5.5_45.5,11.5:800x600"
+grid_output <- grid(startdate, parameter, coordinate)
+head(grid_output)
+
+
+#Pivot Grid####
+
+parameter <- "t_2m:C" #only one parameter
+coordinate <- "48.5,5.5_45.5,11.5:5x5" #Rectangle
+pivot_grid <- grid_pivot(startdate, parameter, coordinate)
+head(pivot_grid)
+
+
+#Timeseries grid####
+
+parameter <- "t_2m:C"
+coordinate <- "50,0_40,20:500x300"
+interval <- "PT12H"
+timeseries_grid_output <- timeseries_grid(startdate, enddate, interval, parameter, coordinate)
+head(timeseries_grid_output) 
+
+
+#Get a list of lightning strikes####
+
+time_range="2019-07-07T12:00:00ZPT2S"
+bounding_box="50,10_40,20"
+
+ls <- lightning_strikes(time_range, bounding_box)
+head(ls)
+
+
+#save timeseries grid as .netcdf####
+
+parameters = "t_2m:C"
+coordinate = "90,-180_-90,180:600x400"
+startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                         month = as.integer(strftime(today(),'%m')), 
+                         day = as.integer(strftime(today(),'%d')), 
+                         hour = 00, min = 00, sec = 00, tz = "UTC")
+duration = "PT3H"
+interval = "PT1H"
+
+save_netcdf(startdate, duration, interval, parameters, coordinate)
+
+
+#safe grads plot####
+
+parameters <- "t_500hPa:C,gh_500hPa:m"
+coordinate <- "70,-15_35,30:0.1,0.1"
+
+save_grads_plot(startdate, parameters, coordinate)
+
+
+#Find stations####
+
+location <- "location=47.3,9.3"
+#parameters=<comma separated parameter list> 
+parameters <- "parameters=t_2m:C" 
+stations <- find_station(location, parameters)
+head(stations)
+
+
+#Query a timeseries for a weather station with certain coordinates if possible####
+startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                        month = as.integer(strftime(today(),'%m')), 
+                        day = as.integer(strftime(today(),'%d'))-1, 
+                        hour = 00, min = 00, sec = 00, tz = "UTC")
+enddate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                      month = as.integer(strftime(today(),'%m')), 
+                      day = as.integer(strftime(today(),'%d')), 
+                      hour = 00, min = 00, sec = 00, tz = "UTC")
+
+interval <- "PT1H"
+parameters <- "t_2m:C,relative_humidity_2m:p"
+coordinate <- "47.43,9.4"
+
+stations_timeseries <- station_timeseries(startdate, enddate, interval, parameters, coordinate)
+head(stations_timeseries)
+
+
+#Query a timeseries for a weather station with certain IDs####
+
+startdate = ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                        month = as.integer(strftime(today(),'%m')), 
+                        day = as.integer(strftime(today(),'%d'))-1, 
+                        hour = 00, min = 00, sec = 00, tz = "UTC")
+enddate = ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                      month = as.integer(strftime(today(),'%m')), 
+                      day = as.integer(strftime(today(),'%d')), 
+                      hour = 00, min = 00, sec = 00, tz = "UTC")
+
+interval <- "PT3H"
+parameters = "t_2m:C,relative_humidity_2m:p" 
+station <- "wmo_066810"
+
+timeseries_station_id(startdate, enddate, interval, parameters, station)
+
+
+#Get latest initial times####
+
+parameters <- "t_2m:C,relative_humidity_2m:p" 
+
+model <- "ecmwf-ifs"
+
+startdate = ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
+                        month = as.integer(strftime(today(),'%m')), 
+                        day = as.integer(strftime(today(),'%d')), 
+                        hour = 00, min = 00, sec = 00, tz = "UTC")
+date <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
+
+init_dates(model, date, parameters)
+
+
+#Get available time range####
+
+model <- "ecmwf-ifs"
+paramters <- "global_rad:W,t_2m:C"
+
+available_time_range(model,parameters)
