c93dfb78487def16530f5f4acf09186f1360fd54
Jesús Martin Berlanga <jberlanga@meteomatics.com>
2022-06-20 10:43:36 +0200

    add ens_select options to timeseries

--- R_query_api.R
+++ R_query_api.R
@@ -24,8 +24,8 @@ query_user_features = function(username, password)
 
 #Def Multi Plot Function
 multiplot = function(..., plotlist=NULL, file, cols=1, layout=NULL)  {
- 
- 
+
+
   #Make a list from the ... arguments and plotlist
   plots <- c(list(...), plotlist)
   numPlots <- length(plots)
@@ -55,72 +55,85 @@ multiplot = function(..., plotlist=NULL, file, cols=1, layout=NULL)  {
   }
 }
 
+create_optionals = function(ens_select="")
+{
+  options_str <- ""
+  if(ens_select != "")
+  {
+    options_str <- str_c(options_str, "&ens_select=", ens_select)
+
+  }
+  return(options_str)
+}
 
 #Timeseries
-timeseries <- function(startdate, enddate, interval, parameters, coordinate, plot = FALSE, model="mix", save_png = FALSE, target_directory = paste(getwd(),"timeseries.png",sep ="/"), plot_directory=getwd())
+timeseries <- function(startdate, enddate, interval, parameters, coordinate,
+  plot = FALSE, model="mix", save_png = FALSE,
+  target_directory = paste(getwd(),"timeseries.png",sep ="/"),
+  plot_directory=getwd(), ens_select="")
 {
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   enddate_query <- strftime(enddate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
-  query = sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s", 
-                  username, password, startdate_query, enddate_query, interval, parameters, coordinate, model)
+  query = sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s%s",
+                  username, password, startdate_query, enddate_query, interval,
+                  parameters, coordinate, model, create_optionals(ens_select))
   resp1 <- GET(query)
-  
-  con <- textConnection(content(resp1,"text"))
+
+  content <- content(resp1,"text")
+  header <- str_split(str_sub(content, end=str_locate(content, "\n")[1]-1), ";")
+  con <- textConnection(content)
   parsed1 <- read.csv(con, sep = ";")
   result <- structure(list(content = parsed1))
-  
+
   if (resp1$status_code != 200) {return(result$content)}  else {
-    
+
     df_timeseries <- as.data.frame(result$content)
     df_timeseries$validdate <- as.POSIXct(df_timeseries$validdate,format='%Y-%m-%dT%H:%M:%OSZ', tz = 'UTC')
     df_timeseries$validdate <- with_tz(df_timeseries$validdate, tzone = time_zone)
-  
+
     number_coordinates <- str_count(coordinate, pattern = "\\+")+1
-    
-    
+
+
     if (number_coordinates == 1) {
-      split_list <- strsplit(parameters, ",")
-      
       if (plot == TRUE) {
       plots = NULL
       for (i in names(df_timeseries[2:ncol(df_timeseries)]))  {
         plots[[i]] = ggplot(data=df_timeseries, aes_string(x = df_timeseries$validdate, y = i)) +
         geom_line(aes_string(y = i)) + geom_point(aes_string(y = i)) +
-        labs(x = "Time") 
+        labs(x = "Time")
         }
-  
+
       mp <- multiplot(plotlist = plots)
-    
+
       if (save_png == TRUE) {
       dev.copy(png, target_directory)
       dev.off()
-      
+
       }
       }
-      colnames(df_timeseries) <- c("validdate",split_list[[1]][])
+      colnames(df_timeseries) <- c(header[[1]][])
     }
-  
+
     else {
-    split_list <- strsplit(parameters, ",")
-    colnames(df_timeseries) <- c("lat","lon","validdate",split_list[[1]][])
+    colnames(df_timeseries) <- c(header[[1]][])
     n <- nrow(df_timeseries)
-    df_split <- split(df_timeseries, rep(1:number_coordinates, each=n/number_coordinates))    
+    df_split <- split(df_timeseries, rep(1:number_coordinates, each=n/number_coordinates))
     if (plot == TRUE) {
       for (i in 1:number_coordinates){
         df_timeseries_split <- as.data.frame(df_split[i])
         plots <- NULL
         for (j in names(df_timeseries_split[4:ncol(df_timeseries_split)]))  {
-       
+
           plots[[j]] <- ggplot(data=df_timeseries_split, aes_string(x = df_timeseries_split[,3], y = j)) +
           geom_line(aes_string(y = j)) + geom_point(aes_string(y = j)) +
-          labs(x = "Time") 
+          labs(x = "Time")
           mp <- multiplot(plotlist=plots)
-             
+
           #save png
-          
+
           filename <- paste("timeseries", j, sep="-")
           fullname <- paste(filename, ".png", sep="")
-          
+
           if (save_png == TRUE) {
             current_wd <- getwd()
             setwd(plot_directory)
@@ -128,26 +141,26 @@ timeseries <- function(startdate, enddate, interval, parameters, coordinate, plo
             dev.off()
             setwd(current_wd)
           }
-            
-          } 
+
+          }
         }
       }
     }
    }
-  
+
   return(df_timeseries)
    }
-  
+
 
 #Grid
 grid <- function(startdate, parameter, coordinate, model="mix", plot = FALSE, save_png = FALSE, target_directory = paste(getwd(),"grid.png",sep ="/"))
 {
   startdate_query = strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   ensSelect = {}
-  
+
   query = sprintf("https://%s:%s@api.meteomatics.com/%s/%s/%s/csv?model=%s", username, password, startdate_query, parameter, coordinate, model)
   response <- GET(query)
-  
+
   if (status_code(response) != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
@@ -157,28 +170,28 @@ grid <- function(startdate, parameter, coordinate, model="mix", plot = FALSE, sa
     resp1 <- fread(query, skip=2, fill = TRUE)
     result <- structure(list(content = resp1))
     df = as.data.frame(result$content)
- 
+
     #Generate plot
     r = nrow(df)
     c = ncol(df)
     Longs = as.numeric(colnames(df[2:c]))
-    Lon = rep(c(t(Longs)),r)    
-    df_domain = data.frame(Lon) 
+    Lon = rep(c(t(Longs)),r)
+    df_domain = data.frame(Lon)
     Lat = rep(df[1:r,1], each = c-1)
     df_domain["Lat"] = data.frame(as.numeric(Lat))
     df_domain["Values"] = data.frame(Values = c(t(df[1:r,2:c])))
     mm = matrix(unlist(df_domain["Values"]), ncol = c -1 , byrow = TRUE)
-    
+
   if (plot == TRUE) {
     image(z=t(mm[nrow(mm):1,]))
   }
-  
+
   if (save_png == TRUE) {
     dev.copy(png,target_directory)
     dev.off()
    setwd(current_wd)
   }
- 
+
   colnames(df_domain) <- c("Lon","Lat",parameter)
   return(df_domain)
   }
@@ -190,11 +203,11 @@ grid_pivot <- function(startdate, parameter, coordinate, model="mix", plot = FAL
 {
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   ensSelect <- {}
-  
+
   query <- sprintf("https://%s:%s@api.meteomatics.com/%s/%s/%s/csv?model=%s",
                   username, password, startdate_query, parameter, coordinate, model)
   response <- GET(query)
-  
+
   if (status_code(response) != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
@@ -205,13 +218,13 @@ grid_pivot <- function(startdate, parameter, coordinate, model="mix", plot = FAL
     resp1 <- fread(query, skip=2, fill = TRUE)
     result <- structure(list(content = resp1))
     df <- as.data.frame(result$content)
-  
+
 
    r <- nrow(df)
    c <- ncol(df)
    Longs <- as.numeric(colnames(df[2:c]))
-   Lon <- rep(c(t(Longs)),r)    
-   df_domain <- data.frame(Lon) 
+   Lon <- rep(c(t(Longs)),r)
+   df_domain <- data.frame(Lon)
    Lat <- rep(df[1:r,1], each = c-1)
    df_domain["Lat"] <- data.frame(as.numeric(Lat))
 
@@ -220,12 +233,12 @@ grid_pivot <- function(startdate, parameter, coordinate, model="mix", plot = FAL
    if (plot == TRUE) {
     image(z=t(mm[nrow(mm):1,]))
   }
-  
+
   if (save_png == TRUE) {
     dev.copy(png,target_directory)
     dev.off()
   }
-  
+
   pivot_grid <- with(df_domain, tapply(Values, list(Lat, Lon) , I)  )
   return(pivot_grid)
   }
@@ -235,7 +248,7 @@ grid_pivot <- function(startdate, parameter, coordinate, model="mix", plot = FAL
 #Timeseries grid
 timeseries_grid = function(startdate, enddate, interval, parameter, coordinate, model="mix", plot = FALSE, save_png = FALSE, plot_directory= getwd())
 {
- 
+
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   enddate_query <- strftime(enddate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   ensSelect <- {}
@@ -253,29 +266,29 @@ timeseries_grid = function(startdate, enddate, interval, parameter, coordinate, 
     df_timeseries <- as.data.frame(result$content)
     df_timeseries$validdate <- as.POSIXct(df_timeseries$validdate,format='%Y-%m-%dT%H:%M:%OSZ', tz = 'UTC')
     df_timeseries$validdate <- with_tz(df_timeseries$validdate, tzone = time_zone)
-  
+
 
   df_timeseries <- df_timeseries[order(df_timeseries$validdate),]
   #Image every timestep and safe PNG
   df_ts_split <- split(df_timeseries, df_timeseries$validdate)
   l<-length(df_ts_split)
   times<-unique(df_timeseries$validdate)
-  
+
   if (plot == TRUE) {
-  
+
     for (i in 1:l) {
      df <- as.data.frame(df_ts_split[i])
      lat <- df[,1]
      nlat <- nrow(as.data.frame(unique(lat)))
      value <- df[,4]
      ma <- matrix(value, ncol = nlat)
-    
+
      image(ma)
      title(times[i])
-    
+
      filename <- paste("timeseries grid", i, sep="-")
      fullname <- paste(filename, ".png", sep="")
-    
+
      if (save_png == TRUE) {
       current_wd <- getwd()
       setwd(plot_directory)
@@ -285,17 +298,17 @@ timeseries_grid = function(startdate, enddate, interval, parameter, coordinate, 
       }
     }
   }
-  
+
   colnames(df_timeseries) <- c("Lat", "Lon", "validdate", parameter)
   return(df_timeseries)
-  } 
-} 
-    
+  }
+}
+
 
 #Query and save a Timeseries grid as .netcdf if possible
 save_netcdf <- function(startdate, duration, interval, parameters, coordinate, target_directory = paste(getwd(),"timeseries_grid_netcdf.netcdf",sep ="/") ) {
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
-  query <- sprintf("https://%s:%s@api.meteomatics.com/%s%s:%s/%s/%s/netcdf", 
+  query <- sprintf("https://%s:%s@api.meteomatics.com/%s%s:%s/%s/%s/netcdf",
                   username, password, startdate_query, duration, interval, parameters, coordinate)
   response <- GET(query)
   if (status_code(response) != 200) {
@@ -314,28 +327,28 @@ lightning_strikes <- function(time_range, bounding_box) {
   query = sprintf("https://%s:%s@api.meteomatics.com/get_lightning_list?time_range=%s&bounding_box=%s&format=csv",
                   username, password, time_range, bounding_box)
   response <- GET(query)
-  
+
   if (status_code(response) != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
     result <- structure(list(content = parsed1))
     return(result$content)}
-  
+
   else {
-    
+
     download.file(query, "lightnings.csv", mode="wb")
     lightnings <- read.csv("lightnings.csv",encoding = "UTF-8")
-    
+
     if (nrow(lightnings) == 0) { print("No lightning strikes in this time period.")}
-    
+
     else {
-      
+
       resp1 <- fread(query, skip=2, fill = TRUE)
-      data <- structure(list(content = resp1)) 
+      data <- structure(list(content = resp1))
       cn <- c("Time","Lat","Lon","Stroke_current")
       colnames(data$content)<-cn
       return(as.data.frame(data$content))
-      
+
     }
   }
 }
@@ -344,7 +357,7 @@ lightning_strikes <- function(time_range, bounding_box) {
 #Query and download a grad plot if possible
 save_grads_plot <- function(startdate, parameters, coordinate, target_directory = paste(getwd(),"grad_plot.png",sep ="/")) {
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
-  
+
   query = sprintf("https://%s:%s@api.meteomatics.com/%s/%s/%s/grads?model=ecmwf-ifs",
                   username, password, startdate_query, parameters, coordinate)
   response <- GET(query)
@@ -365,13 +378,13 @@ find_station <- function(location, parameters){
   query <- sprintf("https://%s:%s@api.meteomatics.com/find_station?%s&%s",
                   username, password, location, parameters)
   response <- GET(query)
-  
+
   if (response$status_code != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
     result <- structure(list(content = parsed1))
     return(result$content)}
-  
+
   else {
     download.file(query, "stations.csv", mode="wb")
     stations <- read.csv("stations.csv",encoding = "UTF-8")
@@ -379,7 +392,7 @@ find_station <- function(location, parameters){
     df2 <- as.data.frame(stations[,2])
     df <- cbind(df1,df2)
     df <- paste(df$`stations[, 1]`,df$`stations[, 2]`)
-  
+
     split <- str_split_fixed(df, ";", 14)
     split_df <- as.data.frame(split)
     split_df[,14]<-NULL
@@ -388,7 +401,7 @@ find_station <- function(location, parameters){
     colnames(split_df) <-colnames
 
     return(split_df)
-    
+
   }
 }
 
@@ -397,47 +410,47 @@ find_station <- function(location, parameters){
 station_timeseries <- function(startdate, enddate, interval, parameters, coordinate, model='mix-obs') {
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   enddate_query <- strftime(enddate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
-  
-  query <- sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s", 
+
+  query <- sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s",
                 username, password, startdate_query, enddate_query, interval, parameters, coordinate, model)
   response <- GET(query)
-  
+
   if (response$status_code != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
     result <- structure(list(content = parsed1))
     return(result$content)}
-  
+
   else {
     resp1 <- fread(query, skip=2, fill = TRUE)
-    data <- structure(list(content = resp1)) 
+    data <- structure(list(content = resp1))
     df_timeseries <- as.data.frame(data$content)
     split_list <- strsplit(parameters, ",")
     colnames(df_timeseries) <- c("validdate",split_list[[1]][])
     return(df_timeseries)
-  } 
+  }
 }
 
 
 #Station WMO Metar
 timeseries_station_id <- function(startdate, enddate, interval, parameters, station, model='mix-obs'){
-  
+
   startdate_query <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
   enddate_query <- strftime(enddate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
-  
-  query <- sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s", 
+
+  query <- sprintf("https://%s:%s@api.meteomatics.com/%s--%s:%s/%s/%s/csv?model=%s",
                   username, password, startdate_query, enddate_query, interval, parameters, station, model)
   response <- GET(query)
-  
+
   if (response$status_code != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
     result <- structure(list(content = parsed1))
     return(result$content)}
-  
+
   else {
     resp1 <- fread(query, skip=2, fill = TRUE)
-    data <- structure(list(content = resp1)) 
+    data <- structure(list(content = resp1))
     df_timeseries <- as.data.frame(data$content)
     split_list <- strsplit(parameters, ",")
     colnames(df_timeseries) <- c("validdate",split_list[[1]][])
@@ -452,18 +465,18 @@ init_dates <- function(model,date,parameters) {
   query <- sprintf("https://%s:%s@api.meteomatics.com/get_init_date?model=%s&valid_date=%s&parameters=%s",
                    username, password, model, date, parameters)
   response <- GET(query)
-  
+
   if (response$status_code != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
     result <- structure(list(content = parsed1))
     return(result$content)}
-  
+
   else {
     resp1 = fread(query, fill = TRUE)
     data<-structure(list(content = resp1))
     df <- as.data.frame(data)
-  
+
     #Set column names
     split_list <- strsplit(parameters, ",")
     colnames(df) <- c("validdate",split_list[[1]][])
@@ -478,13 +491,13 @@ available_time_range <- function(model, paramters){
   query = sprintf("https://%s:%s@api.meteomatics.com/get_time_range?model=%s&parameters=%s",
                   username, password, model, paramters)
   response <- GET(query)
-  
+
   if (response$status_code != 200) {
     con <- textConnection(content(response,"text"))
     parsed1 <- read.csv(con, sep = ";")
     result <- structure(list(content = parsed1))
     return(result$content)}
-  
+
   else {
     download.file(query, "available_time_range.csv", mode="wb")
     time_range<-read.csv("available_time_range.csv")
--- example.R
+++ example.R
@@ -29,16 +29,25 @@ interval <- "PT1H"
 
 
 #Query a Timeseries####
-
 parameters <- "t_2m:C,relative_humidity_1000hPa:p"
 coordinate <- "47.11,11.47"
 #coordinate <- "47.11,11.47+50,10"
 ts_output <- timeseries(startdate, enddate, interval, parameters, coordinate)
 head(ts_output)
 
+#Query ensemble Timeseries####
+parameters <- "t_2m:C"
+coordinate <- "47.11,11.47"
+ts_output <- timeseries(startdate, enddate, interval, parameters, coordinate, model="ecmwf-vareps", ens_select="member:1-2")
+head(ts_output)
+
+#Query ensemble Timeseries multiple coordinates####
+parameters <- "t_2m:C"
+coordinate <- "47.11,11.47+46.11,10.47"
+ts_output <- timeseries(startdate, enddate, interval, parameters, coordinate, model="ecmwf-vareps", ens_select="member:1-2")
+head(ts_output)
 
 #Grid####
-
 parameter <- "t_2m:C"
 coordinate <- "48.5,5.5_45.5,11.5:800x600"
 grid_output <- grid(startdate, parameter, coordinate)
@@ -46,116 +55,88 @@ head(grid_output)
 
 
 #Pivot Grid####
-
 parameter <- "t_2m:C" #only one parameter
 coordinate <- "48.5,5.5_45.5,11.5:5x5" #Rectangle
 pivot_grid <- grid_pivot(startdate, parameter, coordinate)
 head(pivot_grid)
 
-
 #Timeseries grid####
-
 parameter <- "t_2m:C"
 coordinate <- "50,0_40,20:500x300"
 interval <- "PT12H"
 timeseries_grid_output <- timeseries_grid(startdate, enddate, interval, parameter, coordinate)
-head(timeseries_grid_output) 
-
+head(timeseries_grid_output)
 
 #Get a list of lightning strikes####
-
 time_range="2019-07-07T12:00:00ZPT2S"
 bounding_box="50,10_40,20"
-
 ls <- lightning_strikes(time_range, bounding_box)
 head(ls)
 
-
 #save timeseries grid as .netcdf####
-
 parameters = "t_2m:C"
 coordinate = "90,-180_-90,180:600x400"
-startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
-                         month = as.integer(strftime(today(),'%m')), 
-                         day = as.integer(strftime(today(),'%d')), 
+startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')),
+                         month = as.integer(strftime(today(),'%m')),
+                         day = as.integer(strftime(today(),'%d')),
                          hour = 00, min = 00, sec = 00, tz = "UTC")
 duration = "PT3H"
 interval = "PT1H"
-
 save_netcdf(startdate, duration, interval, parameters, coordinate)
 
-
 #safe grads plot####
-
 parameters <- "t_500hPa:C,gh_500hPa:m"
 coordinate <- "70,-15_35,30:0.1,0.1"
-
 save_grads_plot(startdate, parameters, coordinate)
 
-
 #Find stations####
-
 location <- "location=47.3,9.3"
-#parameters=<comma separated parameter list> 
-parameters <- "parameters=t_2m:C" 
+parameters <- "parameters=t_2m:C" #parameters=<comma separated parameter list>
 stations <- find_station(location, parameters)
 head(stations)
 
-
 #Query a timeseries for a weather station with certain coordinates if possible####
-startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
-                        month = as.integer(strftime(today(),'%m')), 
-                        day = as.integer(strftime(today(),'%d'))-1, 
+startdate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')),
+                        month = as.integer(strftime(today(),'%m')),
+                        day = as.integer(strftime(today(),'%d'))-1,
                         hour = 00, min = 00, sec = 00, tz = "UTC")
-enddate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
-                      month = as.integer(strftime(today(),'%m')), 
-                      day = as.integer(strftime(today(),'%d')), 
+enddate <- ISOdatetime(year = as.integer(strftime(today(),'%Y')),
+                      month = as.integer(strftime(today(),'%m')),
+                      day = as.integer(strftime(today(),'%d')),
                       hour = 00, min = 00, sec = 00, tz = "UTC")
-
 interval <- "PT1H"
 parameters <- "t_2m:C,relative_humidity_2m:p"
 coordinate <- "47.43,9.4"
-
 stations_timeseries <- station_timeseries(startdate, enddate, interval, parameters, coordinate)
 head(stations_timeseries)
 
 
 #Query a timeseries for a weather station with certain IDs####
-
-startdate = ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
-                        month = as.integer(strftime(today(),'%m')), 
-                        day = as.integer(strftime(today(),'%d'))-1, 
+startdate = ISOdatetime(year = as.integer(strftime(today(),'%Y')),
+                        month = as.integer(strftime(today(),'%m')),
+                        day = as.integer(strftime(today(),'%d'))-1,
                         hour = 00, min = 00, sec = 00, tz = "UTC")
-enddate = ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
-                      month = as.integer(strftime(today(),'%m')), 
-                      day = as.integer(strftime(today(),'%d')), 
+enddate = ISOdatetime(year = as.integer(strftime(today(),'%Y')),
+                      month = as.integer(strftime(today(),'%m')),
+                      day = as.integer(strftime(today(),'%d')),
                       hour = 00, min = 00, sec = 00, tz = "UTC")
-
 interval <- "PT3H"
-parameters = "t_2m:C,relative_humidity_2m:p" 
+parameters = "t_2m:C,relative_humidity_2m:p"
 station <- "wmo_066810"
-
 timeseries_station_id(startdate, enddate, interval, parameters, station)
 
-
 #Get latest initial times####
-
-parameters <- "t_2m:C,relative_humidity_2m:p" 
-
+parameters <- "t_2m:C,relative_humidity_2m:p"
 model <- "ecmwf-ifs"
-
-startdate = ISOdatetime(year = as.integer(strftime(today(),'%Y')), 
-                        month = as.integer(strftime(today(),'%m')), 
-                        day = as.integer(strftime(today(),'%d')), 
+startdate = ISOdatetime(year = as.integer(strftime(today(),'%Y')),
+                        month = as.integer(strftime(today(),'%m')),
+                        day = as.integer(strftime(today(),'%d')),
                         hour = 00, min = 00, sec = 00, tz = "UTC")
 date <- strftime(startdate,format='%Y-%m-%dT%H:%M:%OSZ', tz='UTC')
-
 init_dates(model, date, parameters)
 
 
 #Get available time range####
-
 model <- "ecmwf-ifs"
 paramters <- "global_rad:W,t_2m:C"
-
 available_time_range(model,parameters)
